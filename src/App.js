6dca3cb22755129d4f2e32f3f57409e221e04c5f
alpacacino <alpacacino@yandex.ru>
2019-02-18 18:38:30 +0300

    Initial commit. Header section almost done.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="header">
+          <div className="header__logo">
+            Drone<span className="header__logo--light">Zone</span>
+          </div>
+          <nav className="main-nav">
+            <a href="/Home" className="main-nav__link">
+              Home
+            </a>
+            <a href="/Home" className="main-nav__link">
+              About
+            </a>
+            <a href="/Home" className="main-nav__link">
+              Pricing
+            </a>
+            <a href="/Home" className="main-nav__link">
+              Our drones
+            </a>
+            <a href="/Home" className="main-nav__link">
+              Our realizations
+            </a>
+            <a href="/Home" className="main-nav__link">
+              Contact
+            </a>
+          </nav>
         </header>
+        <section className="hero-section">
+          <div className="hero-section__content">
+            <div className="hero-section__slogan">
+              <h1>Film your event with us!</h1>
+              <p>
+                Pellentesque eget nunc sit amet urna ullamcorper fermentum et eu
+                leo.
+              </p>
+              <p>Nunc vel nibh tempor, pharetra lectus congue, luctus orci. </p>
+            </div>
+            <div className="hero-section__buttons">
+              <button className="main-button">Ask for price</button>
+              <button className="main-button main-button--transparent">
+                Watch video
+              </button>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
