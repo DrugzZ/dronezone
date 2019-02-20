2b7f8b5bded70db570646bb1b593a1f77dc2c79d
alpacacino <alpacacino@yandex.ru>
2019-02-20 20:04:49 +0300

    base layed out to map section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import DroneIcon from "./assets/media/drone_icon.png";
 import DroneTopIcon from "./assets/media/drone1_icon.png";
 import PropelerIcon from "./assets/media/propeler_icon.png";
 import ControlIcon from "./assets/media/control_icon.png";
+import DroneImage from "./assets/media/drone_img.jpg";
+import PosterTop from "./assets/media/caption1_img.jpg";
+import PosterBottom from "./assets/media/caption2_img.jpg";
 
 class App extends Component {
   render() {
@@ -107,6 +110,118 @@ class App extends Component {
           <button className="main-button main-button--blue">
             Ask for price
           </button>
+        </section>
+        <section className="info-section">
+          <div className="info-section__content">
+            <header className="info-section__header">
+              <h1>Nature from the air.</h1>
+              <p>
+                Mauris consequat libero metus, nec ultricies sem efficitur quis.
+                Integer bibendum eget metus ac accumsan. Integer sit amet lacus
+                egestas, semper est quis, viverra ex.
+              </p>
+            </header>
+            <ol className="info-section__list">
+              <li className="info-section__list-item">
+                Pellentesque eget nunc sit amet urna ullamcorper fermentum et eu
+                leo. Nunc vel nibh tempor, pharetra lectus congue, luctus orci.
+              </li>
+              <li className="info-section__list-item">
+                Mauris consequat libero metus, nec ultricies sem efficitur quis.
+                Integer bibendum eget metus ac accumsan.
+              </li>
+              <li className="info-section__list-item">
+                Integer sit amet lacus egestas, semper est quis, viverra ex.
+                Mauris consequat libero metus, nec ultricies sem efficitur quis.
+              </li>
+              <li className="info-section__list-item">
+                Pellentesque eget nunc sit amet urna ullamcorper fermentum et eu
+                leo. Nunc vel nibh tempor, pharetra lectus congue, luctus orci.
+              </li>
+              <li className="info-section__list-item">
+                Mauris consequat libero metus, nec ultricies sem efficitur quis.
+                Integer bibendum eget metus ac accumsan.
+              </li>
+            </ol>
+          </div>
+          <img src={DroneImage} alt="dgi phantom drone" />
+        </section>
+        <section className="portfolio-section">
+          <div className="portfolio-section__video">
+            <button className="portfolio-section__video-controller">
+              <span
+                role="img"
+                aria-label="play"
+                style={{ "padding-left": "3px" }}
+              >
+                ▶️
+              </span>
+            </button>
+            <video src="" poster={PosterTop}>
+              Sorry, your browser doesn't support embedded videos, but don't
+              worry, you can <a href="/about">download it</a>
+              and watch it with your favorite video player!
+            </video>
+          </div>
+          <div className="portfolio-section__description">
+            <header className="info-section__header">
+              <h1>Nature from the air.</h1>
+              <p>
+                Pellentesque eget nunc sit amet urna ullamcorper fermentum et eu
+                leo. Nunc vel nibh tempor, pharetra lectus congue, luctus orci
+                nec ultricies sem efficitur quis. Integer bibendum eget metus ac
+                accumsan.
+              </p>
+            </header>
+            <button className="main-button main-button--blue1">
+              Ask for price
+            </button>
+          </div>
+          <div className="portfolio-section__description">
+            <header className="info-section__header">
+              <h1>City from the air.</h1>
+              <p>
+                Mauris consequat libero metus, nec ultricies sem efficitur quis.
+                Integer bibendum eget metus ac accumsan. Integer sit amet lacus
+                egestas, semper est quis, viverra ex. Pellentesque eget nunc sit
+                amet urna ullamcorper fermentum et eu leo.
+              </p>
+            </header>
+            <button className="main-button main-button--blue1">
+              Ask for price
+            </button>
+          </div>
+          <div className="portfolio-section__video">
+            <button className="portfolio-section__video-controller">
+              <span
+                role="img"
+                aria-label="play"
+                style={{ "padding-left": "3px" }}
+              >
+                ▶️
+              </span>
+            </button>
+            <video src="" poster={PosterBottom}>
+              Sorry, your browser doesn't support embedded videos, but don't
+              worry, you can <a href="/about">download it</a>
+              and watch it with your favorite video player!
+            </video>
+          </div>
+        </section>
+        <section className="newsletter-section">
+          <h1 className="newsletter-section__heading">
+            Sign up for out newsletter
+          </h1>
+          <form className="newsletter-section__form">
+            <input
+              type="email"
+              placeholder="Email address"
+              className="newsletter-section__form-input"
+            />
+            <button type="submit" className="newsletter-section__form-btn">
+              Send
+            </button>
+          </form>
         </section>
       </div>
     );
