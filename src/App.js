935ffb10c600b7d8a0779027e569f99e8d1881f7
alpacacino <alpacacino@yandex.ru>
2019-02-28 18:20:56 +0300

    image optimization and lazy loading MapComponent

--- src/App.js
+++ src/App.js
@@ -1,40 +1,49 @@
-import React, { Component } from "react";
+import React, { lazy, Suspense, useState, useEffect } from "react";
 import "./App.scss";
 
 import HeaderComponent from "./components/HeaderComponent";
 import HeroComponent from "./components/HeroComponent";
-import MapComponent from "./components/MapComponent";
 import IconsComponent from "./components/IconsComponent";
 import InfoComponent from "./components/InfoComponent";
 import PortfolioComponent from "./components/PortfolioComponent";
 import NewsletterComponent from "./components/NewsletterComponent";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <HeaderComponent />
-        <HeroComponent />
-        <IconsComponent />
-        <InfoComponent />
-        <PortfolioComponent />
-        <NewsletterComponent />
-        <MapComponent height="500" />
-        <section className="footer-block">
-          <h2
-            style={{
-              color: "black",
-              textAlign: "center",
-              height: "200px",
-              padding: "2rem"
-            }}
-          >
-            This is the footer section
-          </h2>
-        </section>
-      </div>
-    );
-  }
-}
+const MapComponent = lazy(() => import("./components/MapComponent"));
 
-export default App;
+export default () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!scrolled) setScrolled(true);
+    };
+    window.addEventListener("scroll", handleScroll, true);
+    return window.removeEventListener("scroll", handleScroll);
+  });
+
+  return (
+    <div onTouchStart={() => setScrolled(true)} className="App">
+      <HeaderComponent />
+      <HeroComponent />
+      <IconsComponent />
+      <InfoComponent />
+      <PortfolioComponent />
+      <NewsletterComponent />
+      <Suspense fallback={<p>Loading</p>}>
+        {scrolled && <MapComponent height="500" />}
+      </Suspense>
+      <section className="footer-block">
+        <h2
+          style={{
+            color: "black",
+            textAlign: "center",
+            height: "200px",
+            padding: "2rem"
+          }}
+        >
+          This is the footer section
+        </h2>
+      </section>
+    </div>
+  );
+};
